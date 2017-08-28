ba4c0b2c6903bd87b83d77121a33f750709e3c41
David <david.gies@gmail.com>
2017-08-28 15:53:08 +0200

    Create missings.R

---
+++ missings.R
@@ -0,0 +1,11 @@
+missings <- function(DT) {
+  rown <- nrow(DT)
+  # find sum of missings
+  DT <- RTL4_breaks[, lapply(.SD, function(x) sum(is.na(x)))]
+  
+  # make it more readable and sort variables alphabetically
+  DT <- melt(DT, measure.vars = names(DT), variable.name = "Variabele", value.name = "Missing")[order(as.character(Variabele))]
+  
+  # get proportions
+  DT[, Proportioneel := round(Missing / rown, 3)][]
+}
