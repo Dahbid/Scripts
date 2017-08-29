eba1c07c5be3ad5b8916cf1d807c232cacbf3abe
David <david.gies@gmail.com>
2017-08-29 16:23:35 +0200

    Create plot_missings.R
    Plot missing values in a ggplot raster

---
+++ plot_missings.R
@@ -0,0 +1,22 @@
+plot_missings <- function(DT) {
+  vars <- names(DT)
+  DT2 <- copy(DT) 
+  if (!is.data.table(DT2)) {
+    setDT(DT2)
+  }
+  
+  # convert cells to either the class() or NA
+  DT2[, (vars) := lapply(.SD, function(x) ifelse(!is.na(x), paste(class(x), collapse = '\n'), NA))]
+  DT2[, id := .I]
+  
+  # melt data.table (gives warning that not all measure vars are of the same type)
+  slag <- suppressWarnings(melt(DT2, id.vars = "id", measure.vars = vars) )
+  
+  # plot raster
+  ggplot2::ggplot(data = slag, ggplot2::aes_string(x = "variable", y = "id", text = "value")) +
+    ggplot2::geom_raster(aes_string(fill = "value")) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(x = "", y = "Rij") +
+    ggplot2::guides(fill = ggplot2::guide_legend(title = "Type")) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1))
+}
