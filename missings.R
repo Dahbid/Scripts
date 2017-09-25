000fc279829f9a8b00e82db580f9da7ecc34f137
David <david.gies@gmail.com>
2017-09-25 12:14:49 +0200

    Matrix warning

--- missings.R
+++ missings.R
@@ -14,7 +14,7 @@ missings <- function(DT) {
   } else {
     DT2 <- as.data.table(DT)
     DT2 <- DT2[, lapply(.SD, function(x) sum(is.na(x)))]
-    DT2 <- melt(DT2, measure.vars = names(DT), variable.name = "Variabele", value.name = "Missing")[order(as.character(Variabele))]
+    DT2 <- melt(DT2, measure.vars = names(DT2), variable.name = "Variabele", value.name = "Missing")[order(as.character(Variabele))]
     DT2[, Proportioneel := round(Missing / rown, 3)][]
     
     return(DT2)
