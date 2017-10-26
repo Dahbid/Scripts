d48a18bb43711730a9c2cdf3178674604cc39b07
David <david.gies@gmail.com>
2017-10-26 15:41:34 +0200

    added plots plus deparse(substitute(x))

--- cr_score.R
+++ cr_score.R
@@ -1,6 +1,16 @@
-cr_score <- function(DT, response, models, by) {
+cr_score <- function(DT, label, models, by, FUN) {
   library(Metrics)
+  library(Troep)
+  library(ggplot2)
+  
+  #input quoten
+  label <- deparse(substitute(label))
+  by = deparse(substitute(by))
+  FUN <- deparse(substitute(FUN))
+  
+  huh <- FUN
   FUN <- match.fun(FUN)
+  
   # check if DT is a data.table
   if (!is.data.table(DT)) {
     setDT(DT)
@@ -12,7 +22,7 @@ cr_score <- function(DT, response, models, by) {
   # calculate rmse per model
   output <- list()
   for (i in models) {
-    res <- DT[, .(FUN(get(response), get(i))), by = by] 
+    res <- DT[, .(FUN(get(label), get(i))), by = by] 
     setnames(res, no_group, i)
     
     output[[length(output) + 1]] <- res
@@ -20,5 +30,21 @@ cr_score <- function(DT, response, models, by) {
   
   # reduce list output to data.table
   output <- Reduce(cbind, output)
-  return(output[, !duplicated(names(output)), with = F])
+  
+  if (length(models) > 1) {
+    output <- output[, !duplicated(names(output)), with = FALSE]
+  }
+  
+  output2 <- melt(output, id.vars = by)
+  # plot output2
+  p <- ggplot(data = output2,
+         mapping = aes(x = get(by), y = value, fill = variable, label = round(value, 2))) +
+    geom_bar(stat = "identity", position = "dodge") + 
+    geom_text(position = position_dodge(0.9), vjust = 2) +
+    labs(x = by, y = "Score", title = huh) +
+    guides(fill = guide_legend(title = "Model")) +
+    theme(legend.position = "top")
+  
+  print(p)
+  return(output)
 }
