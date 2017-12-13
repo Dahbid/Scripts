fbe439ba36c38d21aa94b9cdea4e4be5071a6643
David <david.gies@gmail.com>
2017-12-13 16:12:15 +0100

    Understandability

--- update_datatable.R
+++ update_datatable.R
@@ -1,20 +1,20 @@
-update <- function(DT, upDT, cols, id) {
+update <- function(original_data, upDT, cols, id) {
   suppressPackageStartupMessages(library(data.table))
-  if (!is.data.table(DT)) {
-    setDT(DT)
+  if (!is.data.table(original_data)) {
+    setDT(original_data)
   }
-  if (!is.data.table(upDT)) {
-    setDT(DT)
+  if (!is.data.table(updated_data)) {
+    setDT(original_data)
   }
   
-  if (!cols %in% names(DT) & !cols %in% names(upDT)) {
+  if (!columns_to_update %in% names(DT) & !columns_to_update %in% names(updated_data)) {
     stop("Column name(s) not found in one or both given data.tables.")
   }
   
-  setkeyv(DT, id)
-  setkeyv(upDT, id)
-  DT[upDT, (cols) := setDT(mget(paste0('i.', cols)))]
+  setkeyv(original_data, id)
+  setkeyv(updated_data, id)
+  original_data[updated_data, (columns_to_update) := setDT(mget(paste0('i.', columns_to_update)))]
   
-  setkey(DT, NULL)
-  setkey(upDT, NULL)
+  setkey(original_data, NULL)
+  setkey(updated_data, NULL)
 }
